564ca708ffded9318fa2f9263695b78595c7e910
Nicolas Carrière <nicolascarriere15@gmail.com>
2023-01-26 11:59:48 -0500

    changement du title de indez.php et nettoyage du code

--- header.php
+++ header.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>mon 4W4</title>
+
+    <title>mon site web</title>
+
     <?php wp_head(); ?> <!-- Generer l'entente de notre page -->
+
 </head>
 <body>
