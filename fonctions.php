8db657d73bb564769efcdd78bba790006f58913b
Nicolas Carrière <nicolascarriere15@gmail.com>
2023-01-26 11:26:02 -0500

    s1c2 ajout de add_action pour enfiller style.css dans fonction.php

--- fonctions.php
+++ fonctions.php
@@ -2,9 +2,13 @@
 /**
  * L'ensemble des fonctions du thème
  */
-
-    wp_enqueue_style('4w4-gr2-principal', // identificateur
+   function enfiler_css() {
+    wp_enqueue_style('4w4-gr1-principal', // identificateur
                       get_template_directory_uri() . '/style.css', // adresse url de style.css
                       array(), // definir les dépendances
                       filemtime(get_template_directory() . '/style.css'), // Le calcul de la version du fichier css
                       'all'); // media
+
+   }
+
+   add_action( 'wp_enqueue_scripts', 'enfiler_css' );
--- index.php
+++ index.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>mon 4W4</title>
-    <?php wp_head() ?> <!-- Generer l'entente de notre page -->
-</head>
-<body>
-    <h1>Bienvenu sur 4W4</h1>
+<?php 
+/**
+ * index.php est le modèle par default du thème 4w4
+ *  */ ?>
+
+    <?php get_header(); ?>
+
+        <main>
+            <h1>Bienvenu sur 4W4</h1>
+        </main>
+
+    <?php get_footer(); ?>   
+
 </body>
-</html>
+</html
