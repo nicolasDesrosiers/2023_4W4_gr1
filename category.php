2f3f184df657931f7990c2bf1defeed3a5a0aec5
Nicolas Carrière <nicolascarriere15@gmail.com>
2023-02-16 11:17:09 -0500

    s4c2 ajout des categories et inclusions de la page blocflex

--- category.php
+++ category.php
@@ -1,6 +1,7 @@
 <?php 
 /**
- * index.php est le modèle par default du thème 4w4
+ * category.php est le modèle par default 
+ * pour afficher une archive d'article de categrie spécifique
  *  */ 
 ?>
 
@@ -8,7 +9,7 @@
 
         <main>
 
-            <pre>front-page.php</pre>
+            <pre>category-page.php</pre>
             <h1>Bienvenue sur 4W4</h1>
             <section class="blocflex">
             <?php 
@@ -16,7 +17,6 @@
                     while(have_posts()): the_post(); // extrait un objet "post" ?> 
                         <article>
                         <h3><a href="<?php the_permalink(); ?>"><?php the_title('<h3>','</h3>'); // affiche le titre du post ?></a></h3>
-                        <?php //get_the_excerpt();?> 
                         <hr>
                         <?= wp_trim_words(get_the_excerpt(), 10," ... ");?>
                         </article>
