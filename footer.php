4e4ac2eb3aaecccf61bdfbd163d5785490b86e6a
Nicolas Desrosiers <nicolascarriere15@gmail.com>
2023-03-16 11:49:03 -0400

    ajout du menu et du logo dans le footer

--- footer.php
+++ footer.php
@@ -1,8 +1,20 @@
 <footer class="site__footer">
 
-    <scetion class="lien">Lien #1</scetion>
-    <scetion class="lien">Lien #2</scetion>
-    <scetion class="lien">Lien #3</scetion>
+    <scetion class="lien"> <?php the_custom_logo();?> </scetion>
+
+    <scetion class="lien"> <?php wp_nav_menu(array(
+                    "menu" => "entete",
+                    "container" => "nav",
+                    "container_class" => "menu__entete")); ?>
+    </scetion>
+
+    <scetion class="lien">
+        <ul>
+            <li>Nicolas Carrière</li>
+            <li>Concepetion d'interface</li>
+            <li>e2137195@cmaisonneuve.qc.ca</li>
+        </ul>
+    </scetion>
 
 </footer>
 
