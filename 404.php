d31c042b3af0a7b81920536dfbbd1ba74b9fb458
Nicolas Desrosiers <nicolascarriere15@gmail.com>
2023-03-30 09:45:44 -0400

    s9c2 ajout des menus des notes et des cours dans la page erreur 404

--- 404.php
+++ 404.php
@@ -7,20 +7,30 @@
 
 	<main id="main__erreur" class="site-main erreur" role="main">
 		<section class="error-404">
-			<header class="page-header">
-				<h1>Erreur 404</h1>
-			</header>
+			
+			<h3 class="titre">Erreur 404</h3>
+			
 			<div class="erreur__recherche">
 				<p>Page introuvable, vous pouvez tentez une nouvelle recherche</p>
 				<?php get_search_form(); ?>
             </div>  
-            <div class="choix__cours">
 
+            <div class="choix__cours">
+            <h4>Nos choix de cours</h4>
+            <?php wp_nav_menu(array(
+                    "menu" => "cours",
+                    "container" => "nav",
+                    "container_class" => "menu__cours")); ?> 
 
             </div>
-
+            <h4>Les notes de cours</h4>
             <div class="notes__cours">
+            <?php wp_nav_menu(array(
+                    "menu" => "note-4w4",
+                    "container" => "nav",
+                    "container_class" => "menu__note")); ?> 
 
+            </div>    
 
             </div>
 
