b4daf2571cd32929a37ea0a7c48a7b1949098d20
Nicolas Desrosiers <nicolascarriere15@gmail.com>
2023-04-20 12:30:45 -0400

    s12c1 début widget, ajout de la fucntion et ajout du widget dans footer

--- footer.php
+++ footer.php
@@ -1,19 +1,24 @@
 <footer class="site__footer">
 
-    <section class="lien"> <?php the_custom_logo();?> </section>
 
-    <section class="info">
-        <ul>
-            <li>Nicolas Carrière</li>
-            <li>Concepetion d'interface</li>
-            <li>e2137195@cmaisonneuve.qc.ca</li>
-        </ul>
+    <section class="footer_colonne">
+      <div class="sidebar">
+      <?php dynamic_sidebar( 'footer_1' ); ?>
+      </div>
     </section>
 
-    <section class="lien"> <?php wp_nav_menu(array(
-                    "menu" => "liens",
-                    "container" => "nav",
-                    "container_class" => "menu__liens")); ?>
+
+    <section class="footer_colonne">
+      <div class="sidebar">
+      <?php dynamic_sidebar( 'footer_2' ); ?>
+      </div>
+    </section>
+
+
+    <section class="footer_colonne">
+      <div class="sidebar">
+      <?php dynamic_sidebar( 'footer_3' ); ?>
+      </div>
     </section>
 
 
--- functions.php
+++ functions.php
@@ -78,3 +78,40 @@ if($args->menu == 'note-4w4') { // on filtre uniquement le menu «note-4w4»
 return $title;
 }
 add_filter('nav_menu_item_title', 'perso_menu_item_title', 10, 3);    
+
+
+/** ------------------------------------------------------------- Enregistrement des Widget */
+
+// Enregistrer le sidebar
+function enregistrer_sidebar() {
+        register_sidebar( array(
+            'name' => __( 'Footer 1', 'nom-de-mon-theme' ),
+            'id' => 'footer_1',
+            'description' => __( 'Une zone de widget pour afficher des widgets dans le pied de page.', 'nom-de-mon-theme' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget' => '</div>',
+            'before_title' => '<h2 class="widget-title">',
+            'after_title' => '</h2>',
+        ) );
+
+        register_sidebar( array(
+                'name' => __( 'Footer 2', 'nom-de-mon-theme' ),
+                'id' => 'footer_2',
+                'description' => __( 'Une zone de widget pour afficher des widgets dans le pied de page.', 'nom-de-mon-theme' ),
+                'before_widget' => '<div id="%1$s" class="widget %2$s">',
+                'after_widget' => '</div>',
+                'before_title' => '<h2 class="widget-title">',
+                'after_title' => '</h2>',
+            ) );
+
+        register_sidebar( array(
+                'name' => __( 'Footer 3', 'nom-de-mon-theme' ),
+                'id' => 'footer_3',
+                'description' => __( 'Une zone de widget pour afficher des widgets dans le pied de page.', 'nom-de-mon-theme' ),
+                'before_widget' => '<div id="%1$s" class="widget %2$s">',
+                'after_widget' => '</div>',
+                'before_title' => '<h2 class="widget-title">',
+                'after_title' => '</h2>',
+            ) );
+    }
+    add_action( 'widgets_init', 'enregistrer_sidebar' );
