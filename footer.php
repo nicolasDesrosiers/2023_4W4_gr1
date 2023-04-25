f8f8a0a0df2c2558bf0462ab4aa1fc03d3bab556
Nicolas Desrosiers <nicolascarriere15@gmail.com>
2023-04-25 19:04:12 -0400

    s12c2 Modification des widget et rectification du sass pour mettre le footer sur deux colonnes

--- footer.php
+++ footer.php
@@ -1,5 +1,31 @@
 <footer class="site__footer">
 
+<section class="colonne">
+
+    <section class="lien"> <?php wp_nav_menu(array(
+                    "menu" => "liens",
+                    "container" => "nav",
+                    "container_class" => "menu__liens")); ?>
+    </section>
+
+    <section class="info">
+        <ul>
+            <li>Nicolas Carrière</li>
+            <li>Concepetion d'interface</li>
+            <li>e2137195@cmaisonneuve.qc.ca</li>
+        </ul>
+    </section>
+
+    <section class="lien"> 
+        <?php the_custom_logo();?> 
+    </section>
+
+
+ 
+
+</section>
+
+<section class="colonne">
 
     <section class="footer_colonne">
       <div class="sidebar">
@@ -7,13 +33,11 @@
       </div>
     </section>
 
-
     <section class="footer_colonne">
       <div class="sidebar">
       <?php dynamic_sidebar( 'footer_2' ); ?>
       </div>
     </section>
-
 
     <section class="footer_colonne">
       <div class="sidebar">
@@ -21,6 +45,7 @@
       </div>
     </section>
 
+</section>
 
 </footer>
 
--- front-page.php
+++ front-page.php
@@ -8,6 +8,14 @@
 
         <main class="site__main">
             <h1>Bienvenue sur 4W4</h1>
+            <h2>Les événements à venir</h2>
+            <section class="blocflex">
+               <?php wp_nav_menu(array(
+                    "menu" => "evenement",
+                    "container" => "nav",
+                    "container_class" => "menu__evenement")); ?> 
+
+               </section>
             <section class="blocflex">
             <?php 
                 if (have_posts()):
