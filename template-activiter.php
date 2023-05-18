ac6b68e398568c789d8a2a2ddabe1260b5f09a92
Nicolas Desrosiers <nicolascarriere15@gmail.com>
2023-05-18 12:19:53 -0400

    s15c1 Modification du css dans la template activité et ajout des tumbnails dans la page activité

--- template-activiter.php
+++ template-activiter.php
@@ -6,9 +6,12 @@
 ?>
 
 <?php get_header(); ?>
-<main class="site__main">
+<main class="site__main activiter">
 <?php
 if ( have_posts() ) : the_post(); ?>
+<div class="image__activiter">
+    <?php the_post_thumbnail( 'medium_large' ) ?>
+</div>
 <h1><?= get_the_title(); ?></h1>
 <?php the_content();?>
 <p>L'activité :  <?php the_field('activiter'); ?></p>
