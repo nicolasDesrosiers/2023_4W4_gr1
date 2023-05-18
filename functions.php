b8a7bc2aeba2440476677d54d8184ef402dbdd73
Nicolas Desrosiers <nicolascarriere15@gmail.com>
2023-05-18 11:54:02 -0400

    s15c1 modification css des template evenement, ajout du tumbnails dans les pages evenements

--- functions.php
+++ functions.php
@@ -91,7 +91,7 @@ function add_menu_description_and_thumbnail( $item_output, $item, $depth, $args 
              'bloc-archives' == $args->menu ) {
             $post_thumbnail_id = get_post_thumbnail_id( $item->object_id );
             if ( $post_thumbnail_id ) {
-                $post_thumbnail_url = wp_get_attachment_image_src( $post_thumbnail_id, 'thumbnail' );
+                $post_thumbnail_url = wp_get_attachment_image_src( $post_thumbnail_id, 'meduim' );
                 $item_output = str_replace( '">' . $args->link_before . $item->title, '">' . $args->link_before . '<span class="title">' . $item->title . '</span><span class="description">' . $item->description . '</span><img src="' . esc_url( $post_thumbnail_url[0] ) . '" class="menu-thumbnail" />', $item_output );
             } else {
                 $item_output = str_replace( '">' . $args->link_before . $item->title, '">' . $args->link_before . '<span class="title">' . $item->title . '</span><span class="description">' . $item->description . '</span><figure class="img__archive"></figure>', $item_output );
--- template-evenement.php
+++ template-evenement.php
@@ -6,9 +6,12 @@
 ?>
 
 <?php get_header(); ?>
-<main class="site__main">
+<main class="site__main evenement">
 <?php
 if ( have_posts() ) : the_post(); ?>
+<div class="image__evenement">
+    <?php the_post_thumbnail( 'medium_large' ) ?>
+</div>
 <h1><?= get_the_title(); ?></h1>
 <?php the_content();?>
 <p>L'adresse de l'evénement : <?php the_field('adresse'); ?></p>
